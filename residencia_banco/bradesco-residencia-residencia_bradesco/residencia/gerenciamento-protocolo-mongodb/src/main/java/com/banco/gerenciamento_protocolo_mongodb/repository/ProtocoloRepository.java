20482969d3e4f746f193a413d26c23763ecefe87
Caio <161636608+Caio-Queiroz@users.noreply.github.com>
2024-10-14 20:27:34 -0300

    Add files via upload

---
+++ residencia_banco/bradesco-residencia-residencia_bradesco/residencia/gerenciamento-protocolo-mongodb/src/main/java/com/banco/gerenciamento_protocolo_mongodb/repository/ProtocoloRepository.java
@@ -0,0 +1,17 @@
+package com.banco.gerenciamento_protocolo_mongodb.repository;
+
+
+import com.banco.gerenciamento_protocolo_mongodb.model.Protocolo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProtocoloRepository extends MongoRepository<Protocolo, String> {
+
+    Optional<Protocolo> findByNumeroProtocolo(String numeroProtocolo);
+
+    List<Protocolo> findByStatus(String status);
+}
+
